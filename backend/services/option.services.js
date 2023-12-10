f1513db297cb0d1e75efba0820a47b49e4f2abf7
hajalimazen__ <hadjalimazen@gmail.com>
2023-12-10 23:11:18 +0100

    c

--- backend/services/option.services.js
+++ backend/services/option.services.js
@@ -11,8 +11,28 @@ const findAllOptions = async () => {
     return OptionModel.find().populate('fichetechnique optionparent');
 };
 
+// A utility function to recursively populate the suboptions
+// It will attach a new property 'suboptions' to every document populated
+const recursivelyPopulateSuboptions = async (options) => {
+    for (let option of options) {
+        // Populate the 'optionparent' field in the suboptions
+        const suboptions = await OptionModel.find({ optionparent: option._id })
+                                             .populate('fichetechnique optionparent');
+        // Recursively populate suboptions for each suboption
+        option._doc.suboptions = await recursivelyPopulateSuboptions(suboptions);
+    }
+    return options;
+};
+
 const findOptionsByIdfiche = async (id) => {
-    return OptionModel.find({ fichetechnique: id, optionparent: null }).populate('fichetechnique optionparent');
+    // Initially, find the top-level options only (where 'optionparent' field is null)
+    let options = await OptionModel.find({ fichetechnique: id, optionparent: null })
+                                   .populate('fichetechnique optionparent');
+    
+    // Recursively populate 'suboptions' for the top-level options
+    options = await recursivelyPopulateSuboptions(options);
+
+    return options;
 };
 
 const updateOption = async (id, optionData) => {
